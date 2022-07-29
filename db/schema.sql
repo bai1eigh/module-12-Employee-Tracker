2992d09e82a33631e8d3e8fbb0d42e52a93b5e25
Baileigh <bai1eigh@outlook.com>
2022-07-28 20:34:03 -0400

    added primary key

--- db/schema.sql
+++ db/schema.sql
@@ -8,28 +8,30 @@ Use employee_tracker;
 -- Creates the table "department" within employee_tracker --
 CREATE TABLE department (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT,
+  id INT PRIMARY KEY,
   -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(30)
+  name VARCHAR(30),
+
+
 );
 
 -- Creates the table "role" within employee_tracker --
 CREATE TABLE role (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT,
+  id INT PRIMARY KEY,
   -- Makes a string column called "name" which cannot contain null --
   title VARCHAR(30) NOT NULL,
 
   salary Decimal NOT NULL,
 
-  department_id INT
+  department_id INT,
 
 );
 
 -- Creates the table "EMPLOYEE" within employee_tracker --
-CREATE TABLE role (
+CREATE TABLE employee (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT,
+  id INT PRIMARY KEY,
   -- Makes a string column called "name" which cannot contain null --
  first_name VARCHAR(30) NOT NULL,
 
