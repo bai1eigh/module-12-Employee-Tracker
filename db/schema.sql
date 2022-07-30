27be1a7845b09355b347696b0d0eb3622299c6d2
Baileigh <bai1eigh@outlook.com>
2022-07-29 23:29:57 -0400

    input data into seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -7,26 +7,27 @@ Use employee_tracker;
 -- Creates the table "department" within employee_tracker --
 CREATE TABLE department (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
 -- Creates the table "role" within employee_tracker --
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary Decimal NOT NULL,
   department_id INT,
-
+PRIMARY KEY (id)
 );
 
 -- Creates the table "EMPLOYEE" within employee_tracker --
 CREATE TABLE employee (
- id INT PRIMARY KEY,
- first_name VARCHAR(30) NOT NULL,
- last_name VARCHAR(30) NOT NULL,
- role_id  INT NOT NULL,
- manager_id  INT
-
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NULL,
+ last_name VARCHAR(30) NULL,
+ role_id  INT NULL,
+ manager_id  INT,
+PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,39 @@
-INSERT INTO employee_tracker ()
+Use employee_tracker;
 
--- example -- 
-INSERT INTO courses (id, course_title, course_description, active)
-VALUES (001, "Algebra I", "Linear equations, inequalities, functions, and graphs", true),
-       (002, "Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
-       (003, "Calculus I", "Limits, Differentiation, Derivatives", true),
-       (004, "Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 88000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 100000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Financial Analyst", 120000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 120000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 130000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Corbin", "Blue", 001, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Raven", "Symone", 002, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Zac", "Efron", 003, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Miley", "Cyrus", 004, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Demi", "Lovato", 005, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Joe", "Jonas", 006, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Cole", "Sprouse", 007, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dylan", "Sprouse", 008, null);
